441537279b99296c58278e3e1f4dea5e6f0d5940
d2ycode <munawar.adi31@gmail.com>
2021-12-14 10:45:23 +0700

    optimize SEO

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,7 +6,7 @@ const Skills: FunctionComponent<{ value: Skill }> = ({ value: { Icon, name } }) 
   return (
     <div className="grid-cols-1 my-2">
       <div className="flex items-center justify-center w-20 h-20 mx-2 bg-gray-200 md:mx-0 dark:bg-dark-100">
-        <img src={Icon} className="w-14 h-14" alt={name} title={name} />
+        <img src={Icon} width="50" height="50" alt={name} title={name} />
       </div>
     </div>
   );
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -11,13 +11,13 @@ const Resume = () => {
       {/* //! Education & Experience */}
       <div className="grid gap-6 md:grid-cols-2">
         <div>
-          <h5 className="px-2 my-3 text-2xl font-bold text-gray-200 border-l-4 border-blue-600">Education</h5>
+          <h1 className="px-2 my-3 text-2xl font-bold border-l-4 border-blue-600 dark:text-gray-200">Education</h1>
           <p className="my-2 text-xl font-bold">Computer Science Engineering</p>
           <p className="font-semibold">Academy of Technology (2017-2021)</p>
           <p className="my-3">I am currently pursuing B.tech in Computer Science Engineering from Academy of Technology</p>
         </div>
         <div>
-          <h5 className="px-2 my-3 text-2xl font-bold text-gray-200 border-l-4 border-blue-600">Experience</h5>
+          <h1 className="px-2 my-3 text-2xl font-bold border-l-4 border-blue-600 dark:text-gray-200">Experience</h1>
           <p className="my-2 text-xl font-bold">Software Developer Jr.</p>
           <p className="font-semibold">Tata Consultancy Services</p>
           <p className="my-3">I dont know why I am doing this job</p>
