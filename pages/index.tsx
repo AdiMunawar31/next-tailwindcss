54c473a44f4ddb9789575ac7e926fa5519591bf9
d2ycode <munawar.adi31@gmail.com>
2022-01-07 16:53:41 +0700

    change bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,7 @@ const About: NextPage = () => {
       <Head>
         <title>Adi Munawar | About</title>
       </Head>
-      <h6 className="my-3 text-base font-medium">
-        I am currently pursuing B.Tech Degree(Final Year) in Computer Science Engineering from Academy of Technology. I have 3+ years of experience in Web Development and I have a Youtube Channel where I teach Full Stack Web Development
-      </h6>
+      <h6 className="my-3 text-base font-medium">Hello There, my name is Adi Munawar, and people who know me call me Adi (20), I was born in Kuningan, July 31, 2001. For now, I live in Mandirancan - Kuningan, West Java, Indonesia</h6>
       <div className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 " style={{ marginLeft: '-1.5rem', marginRight: '-1.5rem' }}>
         <h4 className="my-3 text-xl font-semibold tracking-wide">What I am doing</h4>
 
