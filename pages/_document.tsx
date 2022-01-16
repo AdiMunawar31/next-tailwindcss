57fa11236a531a0cd5b188158431befacb1b1606
d2ycode <munawar.adi31@gmail.com>
2022-01-16 21:56:08 +0700

    change theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-          <meta name="description" content="MERN STACK Developer looking for a job" />
-          <meta name="keywords" content="FullStack Web Developer, Freelancer, MERN Develover" />
+          <meta name='description' content='MERN STACK Developer looking for a job' />
+          <meta name='keywords' content='FullStack Web Developer, Freelancer, MERN Develover' />
+          <meta name='theme-color' content='#2563eb' />
         </Head>
-        <body className="font-sans bg-fixed bg-gradient-to-br from-primary to-gray-300 dark:from-dark dark:to-dark-50 dark:text-gray-400">
+        <body className='font-sans bg-fixed bg-gradient-to-br from-primary to-gray-300 dark:from-dark dark:to-dark-50 dark:text-gray-400'>
           <Main />
           <NextScript />
         </body>
