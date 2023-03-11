d03cd9508448afa1b6f6c82e0f2c37c8191cea6e
AdiMunawar31 <munawar.adi31@gmail.com>
2023-03-11 17:23:03 +0700

    change text description

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,15 @@ const About: NextPage = () => {
       <h6 className="my-3 text-base font-medium">
         {/* Hello There, my name is Adi Munawar, and people who know me call me Adi (21), I was born in Kuningan, July 31,
 				2001. For now, I live in Mandirancan - Kuningan, West Java, Indonesia */}
-        Adi is a Junior Front-End Developer. Experienced with all stages of the development cycle for dynamic mobile and
-        web projects. Well-versed in numerous programming languages including Dart and Javascript.
+        {/* Adi is a Junior Front-End Developer. Experienced with all stages of the development cycle for dynamic mobile and
+        web projects. Well-versed in numerous programming languages including Dart and Javascript. */}
+        Adi is a Junior Developer with basic skills in software development and the ability to build simple applications
+        or systems using the programming language he knows. They are able to test and deploy generated code and have a
+        good understanding of basic software development concepts such as data structures, algorithms, and architectural
+        design. In addition, as a junior developer, Adi has the ability to work in a team, communicate with colleagues,
+        and learn from experience and input provided by senior developers. Adi can complete assigned tasks efficiently
+        with proper supervision and is always ready to learn and develop skills to become a better developer in the
+        future.
       </h6>
       <div
         className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
